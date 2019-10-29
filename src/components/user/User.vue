09b25a1edba5d88f1061e9666dd97e8bc74f7e73
Jose Miralles <jemiralleslopez@gmail.com>
2019-10-29 03:30:13 +0000

    Initial commit

---
+++ src/components/user/User.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>The User Page</h1>
+    <hr />
+    <router-view></router-view>
+    <button @click="navigateToHome" class="btn btn-info">Go to Home</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    navigateToHome() {
+      this.$router.push({ name: "home" });
+    }
+  }
+};
+</script>
